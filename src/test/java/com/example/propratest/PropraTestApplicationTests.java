67ed8e8ba44444a702bc4e1e83170f1f11fb6099
Mawo7503 <marius.wolff03@web.de>
2023-02-22 10:22:13 +0100

    init

---
+++ src/test/java/com/example/propratest/PropraTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.propratest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropraTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
